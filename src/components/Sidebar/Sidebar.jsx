7502dc1be6fb9b796f3528d141fcd4cfdfa52850
codassassin <csouvik130@gmail.com>
2023-10-14 10:59:01 +0530

    updated location settings

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useContext } from 'react';
 import './Sidebar.scss';
 import LogoS from '../../assets/images/logo-lines10p.png';
--- src/context/ApplicationContext.jsx
+++ src/context/ApplicationContext.jsx
@@ -1,16 +1,20 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 import { createContext, useEffect, useState } from "react";
 import devtools from '../../node_modules/devtools-detect/index';
-import os from 'os';
 import { ids } from "../lib/emailJsSources";
 import emailjs from '@emailjs/browser';
+import axios from "axios";
 
 export const ApplicationContext = createContext();
 
 export const ApplicationContextProvider = ({ children }) => {
     const [activeSection, setActiveSection] = useState(null);
     const [forbidden, setForbidden] = useState(false);
+    const [locationForbidden, setLocationForbidden] = useState(false);
     const [letterClass, setLetterClass] = useState('text-animate');
+    const [latitude, setLatitude] = useState(null);
+    const [longitude, setLongitude] = useState(null);
 
     useEffect(() => {
         let timeout;
@@ -27,7 +31,6 @@ export const ApplicationContextProvider = ({ children }) => {
         window.addEventListener('devtoolschange', (event) => {
             if(event.detail.isOpen) {
                 setForbidden(true);
-                sendIPReport();
             }
         });
 
@@ -35,6 +38,24 @@ export const ApplicationContextProvider = ({ children }) => {
             window.removeEventListener('devtoolschange', () => {});
         }
     }, []);
+
+    useEffect(() => {
+        if("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    console.log(position);
+                    setLatitude(position.coords.latitude);
+                    setLongitude(position.coords.longitude);
+                }
+            );
+        } else {
+            return null;
+        }
+    }, []);
+
+    useEffect(() => {
+        sendIPReport();
+    }, [forbidden]);
 
     const handleSectionClick = (sectionId) => {
         document.querySelector(`#${sectionId}`).scrollIntoView({
@@ -45,7 +66,10 @@ export const ApplicationContextProvider = ({ children }) => {
     };
 
     const sendIPReport = async () => {
-        let ip = getIPAddress();
+        let ip = await getIPAddress();
+
+        const pos = `https://www.google.com/maps?q=${latitude},${longitude}`;
+        console.log(pos);
 
         const serviceId = ids.SERVICE_ID;
         const templateId = ids.TEMPLATE_ID;
@@ -55,30 +79,41 @@ export const ApplicationContextProvider = ({ children }) => {
             from_name: ip,
             to_name: "Souvik",
             reply_to: "",
-            message: ip + " tried to acess dev-tools",
+            message: "Someone tried to acess dev-tools with IP address: " + ip + "; Google Map: " + pos,
             subject: "DEV-TOOLS ACCESSED !"
         };
 
-        try {
-            await emailjs
-                .send(serviceId, templateId, templateParams, publicKey);
-        } catch (error) {
-            console.log("Report Sent !!");
+        if(latitude !== null && longitude !== null) {
+            try {
+                await emailjs
+                    .send(serviceId, templateId, templateParams, publicKey);
+            } catch (error) {
+                console.log("Report Sent !!");
+            }
         }
     };
 
-    const getIPAddress = () => {
-        const nets = os.networkInterfaces();
-
-        for(const name of Object.keys(nets)) {
-            for(const net of nets[name]) {
-                if(net.family === 'IPv4' && !net.internal) {
-                    return net.address;
-                }
-            }
+    const getIPAddress = async () => {
+        try {
+            const res = await axios.get("https://geolocation-db.com/json/");
+            return res.data.IPv4;
+        } catch(error) {
+            console.log("REPORT SENT !!");
         }
+    };
 
-        return null;
+    const getGeoLocation = () => {
+        if("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    let pos = `https://www.google.com/maps?q=${position.coords.latitude},${position.coords.longitude}`;
+                    console.log(pos);
+                    return pos;
+                }
+            );
+        } else {
+            return null;
+        }
     };
 
     return (
