2fa13ac0c660945ea7f3e3e0987d148fc786ec6c
codassassin <csouvik130@gmail.com>
2023-10-14 16:52:10 +0530

    updated copy component

--- src/components/About/AboutCont/AboutCont.jsx
+++ src/components/About/AboutCont/AboutCont.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './AboutCont.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight, faEnvelope, faMobileButton } from '@fortawesome/free-solid-svg-icons';
@@ -6,6 +6,34 @@ import { aboutMe, interestsInputs, specializationInputs } from '../../../lib/abo
 import { pdfs } from '../../../lib/assetsSources';
 
 const AboutCont = () => {
+
+    const [emailCopied, setEmailCopied] = useState(false);
+    const [mobileCopied, setMobileCopied] = useState(false);
+
+    const copyToClipboard = (obj) => {
+        navigator.clipboard.writeText(obj);
+    };
+
+    const copyEmailToClipboard = (e) => {
+        e.preventDefault();
+        copyToClipboard(aboutMe.emailId);
+        setMobileCopied(false);
+        setEmailCopied(true);
+        setTimeout(() => {
+            setEmailCopied(false);
+        }, 4000);
+    };
+
+    const copyMobileToClipboard = (e) => {
+        e.preventDefault();
+        copyToClipboard(aboutMe.phoneNo);
+        setEmailCopied(false);
+        setMobileCopied(true);
+        setTimeout(() => {
+            setMobileCopied(false);
+        }, 4000);
+    };
+
     return (
         <div className='aboutCont'>
             <div className='about-cont-row '>
@@ -19,11 +47,17 @@ const AboutCont = () => {
                     <div className='about-cont-row-prop-details-contacts'>
                         <div className='about-cont-row-prop-details-contact'>
                             <FontAwesomeIcon icon={faEnvelope} className='about-cont-row-prop-details-contacts-icon' />
-                            <span>{aboutMe.emailId}</span>
+                            <span onClick={(e) => copyEmailToClipboard(e)}>{aboutMe.emailId}</span>
+                            {emailCopied && (
+                                <div className='copied-component'>Copied !</div>
+                            )}
                         </div>
                         <div className='about-cont-row-prop-details-contact'>
                             <FontAwesomeIcon icon={faMobileButton} className='about-cont-row-prop-details-contacts-icon' />
-                            <span>{aboutMe.phoneNo}</span>
+                            <span onClick={(e) => copyMobileToClipboard(e)}>{aboutMe.phoneNo}</span>
+                            {mobileCopied && (
+                                <div className='copied-component'>Copied !</div>
+                            )}
                         </div>
                     </div>
                 </div>
